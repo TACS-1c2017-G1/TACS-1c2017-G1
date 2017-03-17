3638d20261b655525991f624179e8ce0117dd076
Facundo Aita <facundo91@gmail.com>
2017-03-17 17:44:27 -0300

    Jacoco added

--- src/main/java/com/UTNFRBATACS1c2017/app/Conector.java
+++ src/main/java/com/UTNFRBATACS1c2017/app/Conector.java
@@ -20,6 +20,4 @@ public class Conector {
     }
   }
   
-  public static void main(String[] args) {
-  }
 }
--- src/main/java/com/UTNFRBATACS1c2017/app/GestionadorBusquedas.java
+++ src/main/java/com/UTNFRBATACS1c2017/app/GestionadorBusquedas.java
@@ -2,20 +2,6 @@ package com.UTNFRBATACS1c2017.app;
 
 
 public class GestionadorBusquedas {
-	
-//	TODO este metodo no deberia estar aca creo - Facundo 17/03 
-//	public String fichaPeli(Integer movieCodigo) throws Exception {
-//		return Conector.run("movie/"+movieCodigo.toString(),"");
-//	}
-  
-	public static void main(String[] args) throws Exception {
-//		TODO ELIMINAR, estas son pruebas en la consola para ver la correcta configuracion 
-//		GestionadorBusquedas gestionadorBusquedas = new GestionadorBusquedas();
-//		System.out.println(gestionadorBusquedas.fichaPeli(550));
-//		System.out.println(gestionadorBusquedas.buscarPeliculaPorNombre("Pirates-of-the-Caribbean"));
-//		System.out.println(gestionadorBusquedas.buscarActorPorNombre("Johnny-Depp"));
-//		System.out.println(gestionadorBusquedas.buscarPorNombre("Chaplin"));
-	}
   
 	public String buscarPeliculaPorNombre(String query) throws Exception {
 		return Conector.run("search/movie",query);
