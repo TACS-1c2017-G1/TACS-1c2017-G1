3e54984cf6c428d671b79828f6616d1b6a541ebe
Facundo Aita <facundo91@gmail.com>
2017-03-22 01:52:06 -0300

    Removing key

--- src/main/java/com/UTNFRBATACS1c2017/app/Conector.java
+++ src/main/java/com/UTNFRBATACS1c2017/app/Conector.java
@@ -1,5 +1,8 @@
 package com.UTNFRBATACS1c2017.app;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -7,9 +10,15 @@ import okhttp3.Response;
 public class Conector {
   private static OkHttpClient client = new OkHttpClient();
   private static String apiURL = "https://api.themoviedb.org/3/";
-  private static String api_key = "?&api_key=76e19488d670621ca05341b858d5aa16&query=";  
+  private static String api_key = "";  
   
+  private static void setApi_key(String key) {
+	Conector.api_key = "?&api_key=" + key +"&query=";
+  }
+
   public static String run(String resource, String query) throws Exception {
+	  String key = new String(Files.readAllBytes(Paths.get(".resources")));
+	  Conector.setApi_key(key.substring(0,key.length()-1));
     Request request = new Request.Builder()
         .url(apiURL+resource+api_key+query)
         .get()
@@ -17,7 +26,6 @@ public class Conector {
 
     	Response response = client.newCall(request).execute();
     	return response.body().string();
-
   }
   
 }
