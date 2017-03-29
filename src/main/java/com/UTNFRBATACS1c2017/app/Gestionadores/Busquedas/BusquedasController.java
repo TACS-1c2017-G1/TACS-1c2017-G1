fb5328e780af006ccec07d2c3c24104f42dd8c1f
Facundo Aita <facundo91@gmail.com>
2017-03-29 09:56:03 -0300

    Changing URLs

--- src/main/java/com/UTNFRBATACS1c2017/app/Gestionadores/Busquedas/BusquedasController.java
+++ src/main/java/com/UTNFRBATACS1c2017/app/Gestionadores/Busquedas/BusquedasController.java
@@ -2,30 +2,32 @@ package com.UTNFRBATACS1c2017.app.Gestionadores.Busquedas;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BusquedasController {
 	Busquedas busqueda = new Busquedas();
 
-	@RequestMapping("/searchMovie")
-	public String busquedaPelicula(@RequestParam(value = "query", required = true) String query, Model model)
+	@RequestMapping(value= "/search/movie/{query}", method = RequestMethod.GET)
+	public String busquedaPelicula(@PathVariable String query, Model model)
 			throws Exception {
 		model.addAttribute("movies", busqueda.buscarPeliculaPorNombre(query));
-		return "searchMovie";
+		return "search/movie";
 	}
 
-	@RequestMapping("/searchActor")
-	public String busquedaActor(@RequestParam(value = "name", required = true) String query, Model model)
+	@RequestMapping(value= "/search/person/{query}", method = RequestMethod.GET)
+	public String busquedaActor(@PathVariable String query, Model model)
 			throws Exception {
 		model.addAttribute("movies", busqueda.buscarActorPorNombre(query));
-		return "searchActor";
+		return "search/actor";
 	}
 
-	@RequestMapping("/search")
-	public String busqueda(@RequestParam(value = "name", required = true) String name, Model model) throws Exception {
-		model.addAttribute("movies", busqueda.buscarPorNombre(name));
+	@RequestMapping(value="/search/{query}", method = RequestMethod.GET)
+	public String busqueda(@PathVariable String query, Model model) throws Exception {
+		model.addAttribute("movies", busqueda.buscarPorNombre(query));
 		return "search";
 	}
 
--- src/main/java/com/UTNFRBATACS1c2017/app/others/ActorController.java
+++ src/main/java/com/UTNFRBATACS1c2017/app/others/ActorController.java
@@ -2,14 +2,16 @@ package com.UTNFRBATACS1c2017.app.others;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ActorController {
 
-	@RequestMapping("/actor")
-	public String busquedaPelicula(@RequestParam(value = "id", required = true) String id, Model model)
+	@RequestMapping(value="/actor/{id}", method = RequestMethod.GET)
+	public String busquedaPelicula(@PathVariable String id, Model model)
 			throws Exception {
 		Actor actor = new Actor(id);
 		model.addAttribute("actor", actor);
