c2bffb7bc001c2a3347e254e9641706f94d1ab0f
ayelenMacarena <ayelenmacarenagarcia@gmail.com>
2017-03-29 21:30:55 -0300

    Subo ruta de crear cuenta.

--- src/main/java/app/web/controller/SesionesController.java
+++ src/main/java/app/web/controller/SesionesController.java
@@ -1,5 +1,26 @@
 package app.web.controller;
 
+import app.web.TOs.UserAndPasswordTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/authentication")
 public class SesionesController {
+
+    @RequestMapping(value = "/new-user", method = RequestMethod.POST)
+    @ResponseBody
+    public String newUser(@RequestBody UserAndPasswordTO userAndPassword) throws IOException{
+        //Acá hay que crear el usuario y sino tirar excepción.
+        return "Cuenta creada correctamente!";
+    }
+
+
+
 
 }
