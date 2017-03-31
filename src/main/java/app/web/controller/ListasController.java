1147ecadca5351508101d13f953b2d5be8da5418
JuLuzzi <luzzijulieta.com>
2017-03-30 21:00:22 -0300

    Crear y consultar listas, agregar y eliminar items.

--- src/main/java/app/web/controller/ListasController.java
+++ src/main/java/app/web/controller/ListasController.java
@@ -1,5 +1,51 @@
 package app.web.controller;
 
-public class ListasController {
+import java.util.ArrayList;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import app.model.odb.Movie;
+import app.service.ListasService;
+
+public class ListasController {
+	
+	private ListasService lista = new ListasService();
+	
+	@RequestMapping(value = "/lis{list_id}t", method = RequestMethod.GET)
+	public String consultarLista(@PathVariable int list_id) throws Exception {
+		//
+		return "/list/{lisdt_id}";
+	}
+	
+	@RequestMapping(value = "/list/new-list", method = RequestMethod.POST)
+	public String crearLista(@RequestHeader(value="Token") String Token, @RequestBody String list_name, int list_id) throws Exception {
+		//
+		return "Se creó la lista.";
+	}
+	
+	@RequestMapping(value = "/list/{list_id}/items", method = RequestMethod.POST)
+	public String agregarItem(@RequestHeader(value="Token") String Token, @RequestBody String list_name, int list_id, ArrayList<Movie> movies) throws Exception {
+		//
+		return "Se agregó el item a la lista.";
+	}
+	
+	@RequestMapping(value = "/list/{list_id}/items", method = RequestMethod.DELETE)
+	public String eliminarItem(@RequestHeader(value="Token") String Token, @RequestBody String list_name, int list_id, ArrayList<Movie> movies) throws Exception {
+		//
+		return "Se eliminó el item a la lista.";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
