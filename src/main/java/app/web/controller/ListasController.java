3cc3f6f1bcdd3cadc95bc60badf10a8e8cb843cd
JuLuzzi <luzzijulieta.com>
2017-03-30 23:22:19 -0300

    Crear, consultar Listas, agregar y borrar items (estos dos ultimos no me funcionaron con el objeto de prueba, pero si les paso un String por ejemplo, funciona).

--- src/main/java/app/web/controller/ListasController.java
+++ src/main/java/app/web/controller/ListasController.java
@@ -1,42 +1,46 @@
 package app.web.controller;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import app.model.odb.Movie;
-import app.service.ListasService;
 
+@Controller
 public class ListasController {
-	
-	private ListasService lista = new ListasService();
-	
-	@RequestMapping(value = "/lis{list_id}t", method = RequestMethod.GET)
-	public String consultarLista(@PathVariable int list_id) throws Exception {
+		
+	@RequestMapping(value = "/list/1234", method = RequestMethod.GET)
+	@ResponseBody
+	public String consultarLista(@RequestHeader(value="Token") String token, @PathVariable int list_id) throws Exception {
 		//
-		return "/list/{lisdt_id}";
+		return "/list/{list_id}";
 	}
 	
 	@RequestMapping(value = "/list/new-list", method = RequestMethod.POST)
-	public String crearLista(@RequestHeader(value="Token") String Token, @RequestBody String list_name, int list_id) throws Exception {
+	@ResponseBody
+	public String crearLista(@RequestHeader(value="Token") String Token, @RequestBody String list_name) throws IOException {
 		//
 		return "Se creó la lista.";
 	}
 	
-	@RequestMapping(value = "/list/{list_id}/items", method = RequestMethod.POST)
-	public String agregarItem(@RequestHeader(value="Token") String Token, @RequestBody String list_name, int list_id, ArrayList<Movie> movies) throws Exception {
+	@RequestMapping(value = "/list/1234/itemAgregado", method = RequestMethod.POST)
+	@ResponseBody
+	public String agregarItem(@RequestHeader(value="Token") String token, @RequestBody Movie movie) throws IOException {
 		//
-		return "Se agregó el item a la lista.";
+		return "Se agregó el item.";
 	}
 	
-	@RequestMapping(value = "/list/{list_id}/items", method = RequestMethod.DELETE)
-	public String eliminarItem(@RequestHeader(value="Token") String Token, @RequestBody String list_name, int list_id, ArrayList<Movie> movies) throws Exception {
+	@RequestMapping(value = "/list/1234/itemBorrado", method = RequestMethod.DELETE)
+	@ResponseBody
+	public String eliminarItem(@RequestHeader(value="Token") String Token, @RequestBody Movie movie) throws IOException {
 		//
-		return "Se eliminó el item a la lista.";
+		return "Se eliminó el item.";
 	}
 	
 	
