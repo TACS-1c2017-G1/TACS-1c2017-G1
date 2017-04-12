419d9e53848dd0ec4316d2468d12c56ce7e101c4
Agustín Cipriano <agustincipriano@hotmail.com>
2017-04-12 17:10:46 -0300

    Arreglo el merge

--- src/test/java/app/service/TestUser.java
+++ src/test/java/app/service/TestUser.java
@@ -3,6 +3,7 @@ package app.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,5 +25,29 @@ public class TestUser {
 		listas.add(MovieList.create("Lista 2", pelis));
 		RepositorioDeUsuarios.getInstance().insert(User.create("100", listas));
 		Assert.assertTrue(servicioUsuario.obtenerUsuario("100").getId()==100);
+	}
+	
+	@Test
+	public void testInterseccionListasUsuario(){
+		List<Movie> pelis1 = new ArrayList<Movie>();
+		List<Movie> pelis2 = new ArrayList<Movie>();
+		List<MovieList> listas1 = new ArrayList<MovieList>();
+		List<MovieList> listas2 = new ArrayList<MovieList>();
+		pelis1.add(Movie.create(150,"Matrix"));
+		pelis1.add(Movie.create(100,"Star Wars"));
+		pelis2.add(Movie.create(50,"Toy Story"));
+		pelis2.add(Movie.create(100,"Star Wars"));
+		listas1.add(MovieList.create("Lista 1", pelis1));
+		listas2.add(MovieList.create("Lista 2", pelis2));
+		RepositorioDeUsuarios.getInstance().insert(User.create("100", listas1));
+		RepositorioDeUsuarios.getInstance().insert(User.create("50", listas2));
+		List<Movie> interseccion = new ArrayList<Movie>();
+		interseccion.add(Movie.create(100,"Star Wars"));
+		Assert.assertEquals(servicioUsuario.obtenerInterseccionListas("100", "50"),interseccion);
+	}
+	
+	@After
+	public void repoReset(){
+		RepositorioDeUsuarios.getInstance().clear();
 	}
 }
