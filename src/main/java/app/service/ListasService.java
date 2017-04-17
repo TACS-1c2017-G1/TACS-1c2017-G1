0e1d223c146e74964bec86e3bb38deba2fd9cce7
JuLuzzi <luzzijulieta@gmail.com>
2017-04-17 16:59:29 -0300

    Agrego un test del modelo, MovieList.

--- src/main/java/app/service/ListasService.java
+++ src/main/java/app/service/ListasService.java
@@ -2,11 +2,9 @@ package app.service;
 
 import app.model.odb.Movie;
 import app.model.odb.MovieList;
-import app.model.tmdb.TMDbStatic;
 import app.repositories.RepositorioDeListas;
 import app.repositories.RepositorioDePeliculas;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,10 +36,6 @@ public class ListasService {
 		this.getRepositorioPelicuas().insert(movie);
 		this.consultarLista(id_list, token).addMovie(movie);
 		this.getRepositorioListas().update(movie, id_list);
-	}
-	
-	public static JSONObject buscarPeliculaPorNombreJson(String query) throws Exception {
-		return TMDbStatic.getResource("search/movie", query);
 	}
 
 	public void eliminarItem(Movie movie, int id_list, String token) {
