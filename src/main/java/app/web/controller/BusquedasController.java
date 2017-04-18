0970541c5fc9c670f221f2f48c61c235472ffe53
Facundo Aita <facundo91@gmail.com>
2017-04-18 16:32:42 -0300

    Imports organizadas

--- src/main/java/app/web/controller/BusquedasController.java
+++ src/main/java/app/web/controller/BusquedasController.java
@@ -1,9 +1,15 @@
 package app.web.controller;
 
-import app.service.BusquedasService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import app.service.BusquedasService;
 
 @Controller
 @RequestMapping(value = "/search")
--- src/main/java/app/web/controller/MovieController.java
+++ src/main/java/app/web/controller/MovieController.java
@@ -1,9 +1,15 @@
 package app.web.controller;
 
-import app.model.odb.Movie;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import app.model.odb.Movie;
 
 @Controller
 @RequestMapping(value = "/movie")
