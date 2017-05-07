26e34d196b0fbe8146430de6d14950e25580acd1
Agustín Cipriano <agustincipriano@hotmail.com>
2017-05-07 19:24:18 -0300

    Terminado

--- tacs-frontend/dev/src/services/Usuario.js
+++ tacs-frontend/dev/src/services/Usuario.js
@@ -13,7 +13,7 @@ myApp.service('Usuario', function ($http) {
 
     self.getRecMovies = function (sesion,callback) {
         return $http.get('http://localhost:8080/user/favoriteactor/movies',{
-            headers: {'token': sesionActual.idSesion}
+            headers: {'token': sesion.idSesion}
         }).then(callback);
     }
 
