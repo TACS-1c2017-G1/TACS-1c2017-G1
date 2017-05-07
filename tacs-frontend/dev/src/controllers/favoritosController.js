9cfe35e6b151dbcd8318c3f74fdb90847916ffc7
ayelenMacarena <ayelenmacarenagarcia@gmail.com>
2017-05-07 18:47:13 -0300

    copy paste malo)

--- tacs-frontend/dev/src/controllers/favoritosController.js
+++ tacs-frontend/dev/src/controllers/favoritosController.js
@@ -7,24 +7,7 @@ myApp.controller('favoritosController', function ($rootScope, $scope, Usuario) {
       $scope.actoresFavoritos = actores;
     });
   };
-
-
-  $scope.autenticarse = function () {
-
-    Sesion.login({username: $scope.userName, password: $scope.password})
-      .then(function (response) {
-        $rootScope.sesionActual = response.data;
-        $rootScope.usuarioLogueado = true;
-        $rootScope.esAdmin = response.data.esAdmin;
-
-        $state.go('home');
-      })
-      .catch(function (error) {
-        alert(error.data.message);
-      })
-
-  };
-
+  
   this.actoresFavoritos();
 });
 
