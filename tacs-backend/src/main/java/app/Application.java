edb68e92e3f8040f46e6558149483de566cc8206
Facundo Aita <facundo91@gmail.com>
2017-05-24 20:49:08 -0300

    Simplificando cosas en el frontend

--- tacs-backend/src/main/java/app/Application.java
+++ tacs-backend/src/main/java/app/Application.java
@@ -13,8 +13,8 @@ public class Application {
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
-//		ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
-//		MongoOperations mongoOperation = (MongoOperations)ctx.getBean("mongoTemplate");
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
+		MongoOperations mongoOperation = (MongoOperations)ctx.getBean("mongoTemplate");
 		Initializer.create();
 
 	}
