e232608dacc89736b314b5a2df4fc370e998724b
ayelenMacarena <ayelenmacarenagarcia@gmail.com>
2017-05-25 19:07:36 -0300

    Correcciones antes del PR

--- tacs-backend/src/main/java/app/Application.java
+++ tacs-backend/src/main/java/app/Application.java
@@ -1,9 +1,7 @@
 package app;
 
-import app.repositories.RepositorioDeUsuarios;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -23,8 +21,6 @@ public class Application {
 
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
-    @Autowired
-    private static RepositorioDeUsuarios repositorioDeUsuarios;
 
     public static void main(String[] args) {
 
